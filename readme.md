a8e4e0196dbad61eb8a696d180c9f17f7e49fc71
a-kholmovaia <90838877+a-kholmovaia@users.noreply.github.com>
2023-10-05 13:13:17 +0100

    rewrote data prep

--- readme.md
+++ readme.md
@@ -1,3 +1,6 @@
-- [ ] train
+- [ ] add batch split func (& padding)
+- [ ] split into x & target and create dls
+- [ ] train & visualize
 - [ ] implement lstm
+- [ ] train & visualize
 
---
+++ run.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+script_path="main.py"
+env_name='rnns'
+
+# tell the shell to use the created virtual environment
+source  ~/anaconda3/etc/profile.d/conda.sh
+source activate $env_name
+# install requirements
+pip3 install -r requirements.txt
+# download english model from spacy
+python3 -m spacy download en_core_web_sm
+# Run the script
+# python3 $script_path
+# deactivate the virtual environment
+source deactivate
